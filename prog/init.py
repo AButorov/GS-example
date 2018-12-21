d72f5c298c615da8eedecc316ed53158ab2b82a8
AButorov <40419739+AButorov@users.noreply.github.com>
2018-12-21 23:57:45 +0700

    1

--- prog/init.py
+++ prog/init.py
@@ -1,2 +1,4 @@
  clientId = '65172255284-so2dueuukcmasdcn2sfiphf4rhliea9h.apps.googleusercontent.com'
  clientSecret = ' oPQrBYowiXuhhit1Jn6Uu0N2 '
+
+#  git remote add origin https://github.com/AButorov/qw.git
