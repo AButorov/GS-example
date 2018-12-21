815ae6e30282b059d380a68416fbc99006eb2111
AButorov <40419739+AButorov@users.noreply.github.com>
2018-12-22 03:46:58 +0700

    2

--- prog/init.py
+++ prog/init.py
@@ -1,4 +1,5 @@
  clientId = '65172255284-so2dueuukcmasdcn2sfiphf4rhliea9h.apps.googleusercontent.com'
  clientSecret = ' oPQrBYowiXuhhit1Jn6Uu0N2 '
 
-#  git remote add origin https://github.com/AButorov/qw.git
+#  https://developers.google.com/sheets/api/guides/create
+#  git remote add origin https://github.com/AButorov/xxxxxxx.git
